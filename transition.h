c6634e36be7af5d3b1aa6831047436f1e2058caa
heg66did <heg66did@rhrk.uni-kl.de>
2023-01-28 01:33:18 +0100

    task 2 done

--- transition.h
+++ transition.h
@@ -6,23 +6,31 @@
 #include "place.h"
 
 // Transition:
+template <unsigned int N = 1, unsigned int M = 1>
 SC_MODULE(transition){
 
-    sc_port<placeInterface> in;
-    sc_port<placeInterface> out;
+    sc_port<placeInterface, N, SC_ALL_BOUND> in;
+    sc_port<placeInterface, M, SC_ALL_BOUND> out;
 
     public:
     SC_CTOR(transition){};
 
     void fire(){
-        if(in->testTokens()){
-            std::cout << this->name() << ": Fired" << std::endl;
-            in->removeTokens(1);
-            out->addTokens(1);
+        int k = 0;
+        for (int i = 0; i < N; i++) {
+            if(in[i]->testTokens() == 0)
+                k = 1;
         }
-        else
-            std::cout << this->name() << ": NOT Fired" << std::endl;
 
+        if (k)
+            std::cout << this->name() << ": NOT Fired" << std::endl;
+        else {
+            std::cout << this->name() << ": Fired" << std::endl;
+            for (int i = 0; i < N; i++)
+                in[i]->removeTokens(1);
+            for (int j = 0; j < M; j++)
+                out[j]->addTokens(1);
+        }
     }
 
 
